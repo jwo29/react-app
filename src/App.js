40a19ac84bb8a10505e69a763cb7d50e47587f54
jwo29 <55731054+jwo29@users.noreply.github.com>
2020-03-03 02:31:27 +0900

    spa
    set routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './pages/Home';
+import Judge from './pages/Judge';
+import { HashRouter, Route } from 'react-router-dom';
 
 // function App() {
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HashRouter>
+          <Route exact path="/" component={Home} />
+          <Route path="/judge" component={Judge} />
+        </HashRouter>
+        
       </div>
     );
   }
