fa2b886a1099715fa4fef198a1a4f7d93738e7e7
LeeJiwoo <jwo2955@gmail.com>
2020-03-09 23:40:58 +0900

    in-app-browser open control

--- src/components/CheckBrowser.js
+++ src/components/CheckBrowser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { saveAs } from 'file-saver';
 import * as html2canvas from 'html2canvas';
 import * as Bowser from "bowser";
@@ -6,27 +6,13 @@ import * as Bowser from "bowser";
 const handleClick = () => {
     const browser = Bowser.getParser(window.navigator.userAgent);
 
-    document.write(`The current browser name is "${browser.getBrowserName()}"`);
+    document.write(`The current browser name is "${browser.getBrowserName()}"<br>`);
 
     const browserName = browser.getBrowserName();
-    const browserVersion = browser.getBrowserVersion();
 
-    document.write(browserName);
-    document.write(browserVersion);
-
-    if(browserName === 'Microsoft Edge'){
-        document.write('sorry, Edge is not available');
-    } else if(browserName === 'Chrome') {
-        let body = document.querySelector('body');
-
-        html2canvas(body).then(canvas => {
-            
-            canvas.toBlob((blob) => {
-                saveAs(blob, "result.png");
-            });
-            
-        });
-    } else if(browserName == 'NAVER Whale Browser'){
+    if (browserName === 'Chrome' 
+    || browserName === 'Opera'){
+    // || browserName == 'NAVER Whale Browser'){
         let body = document.querySelector('body');
 
         html2canvas(body).then(canvas => {
@@ -37,18 +23,33 @@ const handleClick = () => {
             
         });
     } else {
-        document.write('pleas use another brower! ex Chrome');
+        // Microsoft Edge
+        // Internet Explorer
+        document.write('pleas use another brower! ex) Chrome');
     }
+
+    // // 결과 캡처 버튼 이벤트 함수
+    // const captureButtonClick = () => {
+    //     html2canvas(document.querySelector("#root"), {
+    //         allowTaint: true,
+    //         useCORS: true
+    //     }).then(canvas => {
+    //         let download = document.getElementById("captureDownload");
+    //         download.href = canvas
+    //             .toDataURL("image/png")
+    //             .replace("image/png", "image/octet-stream");
+    //         download.download = "점심 판결 결과.png";
+    //         download.click();
+    //     });
+    // };
 }
 
 const CheckBrowser = () => {
     return (
         <div>      
             <h1>hello!</h1>
-            <p className="result"></p>
-            <button onClick={handleClick}> 
-                Detect browser 
-            </button>
+            <button onClick={handleClick}>Click me!</button>
+            <script> location.href='intent://www.jwo29.github.io/react-app/#Intent;scheme=http;package=com.android.chrome;end' </script>
         </div>
     );
 }
